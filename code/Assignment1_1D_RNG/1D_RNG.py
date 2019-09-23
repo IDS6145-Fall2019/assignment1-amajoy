d8e4af257b7b86bd6e1738eb640f1f9c4e296620
amajoy <54554498+amajoy@users.noreply.github.com>
2019-09-22 23:12:02 -0400

    Adding Quasi separately.

--- code/Assignment1_1D_RNG/1D_RNG.py
+++ code/Assignment1_1D_RNG/1D_RNG.py
@@ -9,14 +9,13 @@ Created on Sun Sep 22 12:53:36 2019
 
 import matplotlib.pyplot as plt
 import numpy as np
-import chaospy as cp
 
 #Pseudo-Random Uniform Distributions
 
 plt.subplot(3, 5, 1)
 x100 = [np.random.uniform(0,2,100)]
 plt.title("N=100")
-plt.ylabel("Uniform")
+plt.ylabel("P_Uniform")
 plt.hist(x100)
 plt.yticks([])
 plt.xticks([])
@@ -54,7 +53,7 @@ plt.xticks([])
 
 plt.subplot(3, 5, 6)
 x100 = [np.random.exponential(2,100)]
-plt.ylabel("Exponential")
+plt.ylabel("P_Exponential")
 plt.hist(x100)
 plt.yticks([])
 plt.xticks([])
@@ -89,7 +88,7 @@ plt.xticks([])
 
 plt.subplot(3, 5, 11)
 x100 = np.random.binomial(10, 0.5, 100)
-plt.ylabel("Binomial")
+plt.ylabel("P_Binomial")
 plt.hist(x100)
 plt.yticks([])
 plt.xticks([])
@@ -118,175 +117,6 @@ plt.hist(x5k)
 plt.yticks([])
 plt.xticks([])
 
-plt.show()
 plt.savefig('Plotting_1D_RNG_Pseudo.png')
 
 
-#Quasi-Randomness
-#Uniform Distribution 
-
-
-plt.subplot(3, 5, 1)
-uniform = cp.J(cp.Uniform(0,1))
-x100 = uniform.sample(100,rule="S")
-plt.hist(x100, density=True, bins=30)
-plt.title("N=100")
-plt.ylabel("Uniform")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 2)
-uniform = cp.J(cp.Uniform(0,1))
-x500 = uniform.sample(500,rule="S")
-plt.hist(x500, density=True, bins=30)
-plt.title("N=500")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 3)
-uniform = cp.J(cp.Uniform(0,1))
-x1k = uniform.sample(1000,rule="S")
-plt.hist(x1k, density=True, bins=30)
-plt.title("N=1000")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 4)
-uniform = cp.J(cp.Uniform(0,1))
-x2k = uniform.sample(2000,rule="S")
-plt.hist(x2k, density=True, bins=30)
-plt.title("N=2000")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 5)
-uniform = cp.J(cp.Uniform(0,1))
-x5k = uniform.sample(5000,rule="S")
-plt.hist(x5k, density=True, bins=30)
-plt.title("N=5000")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-#Exponential Distribution
-
-
-
-
-plt.subplot(3, 5, 6)
-uniform = cp.J(cp.Exponential())
-x100 = uniform.sample(100,rule="S")
-plt.hist(x100, density=True, bins=30)
-plt.ylabel("Exponential")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 7)
-uniform = cp.J(cp.Exponential())
-x500 = uniform.sample(500,rule="S")
-plt.hist(x500, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 8)
-uniform = cp.J(cp.Exponential())
-x1k = uniform.sample(1000,rule="S")
-plt.hist(x1k, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 9)
-uniform = cp.J(cp.Exponential())
-x2k = uniform.sample(2000,rule="S")
-plt.hist(x2k, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 10)
-uniform = cp.J(cp.Exponential())
-x5k = uniform.sample(5000,rule="S")
-plt.hist(x5k, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-#Normal Distribution - I can't find any binomial distrobution docs Sad face
-
-plt.subplot(3, 5, 11)
-uniform = cp.J(cp.Normal(0,1))
-x100 = uniform.sample(100,rule="S")
-plt.hist(x100, density=True, bins=30)
-plt.ylabel("Normal")
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-
-plt.subplot(3, 5, 12)
-uniform = cp.J(cp.Normal(0,1))
-x500 = uniform.sample(500,rule="S")
-plt.hist(x500, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 13)
-uniform = cp.J(cp.Normal(0,1))
-x1k = uniform.sample(1000,rule="S")
-plt.hist(x1k, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 14)
-uniform = cp.J(cp.Normal(0,1))
-x2k = uniform.sample(2000,rule="S")
-plt.hist(x2k, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-
-
-
-plt.subplot(3, 5, 15)
-uniform = cp.J(cp.Normal(0,1))
-x5k = uniform.sample(5000,rule="S")
-plt.hist(x5k, density=True, bins=30)
-plt.yticks([])
-plt.xticks([])
-
-plt.show()
-plt.savefig("Plotting_1D_RNG_Quasi.png")
